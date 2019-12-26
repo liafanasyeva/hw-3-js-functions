d76ccb1dde48a1aa83c510c0b5e9063117d96926
AfanasyevaElizaveta <55109134+liafanasyeva@users.noreply.github.com>
2019-12-26 23:06:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -40,3 +40,44 @@ function reverseWord(str) {
 }
 
 reverseWord(' A fun little challenge! ')
+
+// Task 5
+function stringExpansion (str) {
+    var strs = [];
+    var nums = [];
+    for (var i = 0; i<str.length;i++ ) {
+        var char = str[i];
+        if (isNaN(char)) {
+            if (nums.length == 0) {
+                strs.push(char);
+            } else {
+            if (nums.length != 0)     
+            for (var j=0; j < nums[0]; j++) {
+            strs.push(char);
+            }
+          }
+        } else { 
+            nums.unshift(char); 
+        }
+    } 
+    console.log(strs.join(''));
+}
+stringExpansion('3D2a5d2f');
+stringExpansion('3d332f2a');
+stringExpansion('abcde');
+
+
+// Task 6
+function largest (...args) {
+    var arr = Array.prototype.slice.call(args);
+    var max = Math.max.apply(null, arr);
+    console.log(max);
+}
+largest(2, 0.1, -5, 100, 3) 
+
+function smallest (...args) {
+    var arr = Array.prototype.slice.call(args);
+    var max = Math.min.apply(null, arr);
+    console.log(max);
+}
+smallest(2, 0.1, -5, 100, 3)
